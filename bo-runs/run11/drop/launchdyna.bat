3c516ca34c358997f7a5f0854a8215a34c979a54
pranavduraisamy <pranavduraisamy@outlook.com>
2026-05-14 16:27:20 +0530

    completed optimization

---
+++ bo-runs/run11/drop/launchdyna.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "d:\Pranav\helmet-fit-bayes-opt\bo-runs\run11\drop"
+"C:\LSDYNA\program\ls-dyna_smp_s_R16.0_619-gd222963e2f_winx64_ifort190_sse2.exe" i=head.key ncpu=20 memory=1500m
+
